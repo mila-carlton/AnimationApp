21f5e113a22e868c286a4dc1f07f226c4f20e46c
mila-carlton <mila.carlton@mail.ru>
2023-11-08 14:42:49 +0400

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by PASGON TEXTILE on 08.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
